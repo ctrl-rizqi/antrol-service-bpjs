3cf341c0fb5fd0e635732023e196be8acd6af6f3
khoirxz <khoirrurrohman@gmail.com>
2026-02-03 13:49:02 +0700

    fix docker

--- apps/service/src/job/poller.scheduler.ts
+++ apps/service/src/job/poller.scheduler.ts
@@ -1,8 +1,11 @@
 import { pollRegisterEvent } from "../poller/registration";
+import { pollQueueRegistration, pollQueueTask } from "../poller/queue";
 
 export function startPollerScheduler() {
   // Jalankan sebagai background worker (looping internal)
   // Jangan gunakan cron karena pollRegisterEvent sudah memiliki while(true)
   pollRegisterEvent();
+  pollQueueRegistration();
+  pollQueueTask();
   console.log("Poller scheduler worker started.");
 }
