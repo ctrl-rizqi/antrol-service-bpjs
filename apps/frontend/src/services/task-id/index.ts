fa140145f613cadea59340ccd67cbb1c37308244
khoirxz <khoirrurrohman@gmail.com>
2026-02-13 14:52:07 +0700

    fix

--- apps/frontend/src/services/task-id/index.ts
+++ apps/frontend/src/services/task-id/index.ts
@@ -40,9 +40,12 @@ export type syncVisitEventResponse = {
 }
 
 export const syncVisitEvent = async (kodebooking: string) => {
-  const response = await api.post<syncVisitEventResponse>('/task-id/manual', {
-    kodebooking,
-  })
+  const response = await api.post<syncVisitEventResponse>(
+    '/visit-event/manual',
+    {
+      kodebooking,
+    },
+  )
 
   return response.data
 }
